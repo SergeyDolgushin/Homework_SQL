a8bbfe3b5f71e9901b847d8c1114d87197f1d8d3
SergeyDolgushin <94533319+SergeyDolgushin@users.noreply.github.com>
2022-02-18 18:19:27 +0300

    version 2

--- pySQL2.py
+++ pySQL2.py
@@ -109,12 +109,26 @@ print(sel)
 
 # 9 название альбомов, содержащих наименьшее количество треков.
 sel = connection.execute("""
-        SELECT album_name, COUNT(album_id) FROM albums al 
+        SELECT album_name, COUNT(album_id) FROM albums al
         JOIN tracks tr ON al.id = tr.album_id
         GROUP BY album_name
         ORDER BY COUNT(album_id)
-        LIMIT 10;
+        LIMIT 1;
         """).fetchmany(100)
 print()
 print(sel)
+# 
 
+sel = connection.execute("""
+        SELECT album_name, COUNT(album_id) FROM albums al
+        JOIN tracks tr ON al.id = tr.album_id
+        GROUP BY album_name, album_id
+        HAVING COUNT(album_id) <= 
+        (SELECT COUNT(album_id) FROM albums al 
+        JOIN tracks tr ON al.id = tr.album_id
+        GROUP BY album_name
+        ORDER BY COUNT(album_id)
+        LIMIT 1);
+        """).fetchmany(100)
+print()
+print(sel)
